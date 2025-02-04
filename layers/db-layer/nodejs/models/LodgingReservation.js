e5c58469bfd1449c103e59b3e49f4abaa21fd1b7
juanmacar <78183588+juanmacar@users.noreply.github.com>
2025-02-04 01:32:09 -0300

    added method to LodgindReservation model and updated the code in wedding_lodging to use path params in all http requests

--- layers/db-layer/nodejs/models/LodgingReservation.js
+++ layers/db-layer/nodejs/models/LodgingReservation.js
@@ -21,6 +21,16 @@ const LodgingReservationSchema = new mongoose.Schema({
     }
 }, { collection: 'lodging_reservations' });
 
+// Define the static method directly on the schema
+LodgingReservationSchema.statics.findByInvitationId = async function(invitationId) {
+    console.log('Searching for lodging reservation with invitationId:', invitationId);
+    const lodgingReservation = await this.findOne({ invitationId });
+    console.log('Found lodging reservation:', lodgingReservation);
+    return lodgingReservation;
+};
+
+// Try to get the existing model first, or create a new one if it doesn't exist
 const LodgingReservation = mongoose.model('Lodging_Reservation', LodgingReservationSchema);
+console.log('Schema statics:', Object.keys(LodgingReservation.schema.statics));
 
 export default LodgingReservation;
